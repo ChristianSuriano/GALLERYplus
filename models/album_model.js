658cdf0e61cce51cbfa5cfff7df881e6de99d076
imvalez <imvalez99@gmail.com>
2024-05-21 00:14:30 +0200

    rm(vb): file models obsoleti

--- models/album_model.js
+++
@@ -1,9 +0,0 @@
-class Album {
-  constructor(title, description, listPhotos, date) {
-    this.id = Math.random();
-    this.title = title;
-    this.description = description;
-    this.listPhotos = listPhotos;
-    this.date = date;
-  }
-}
