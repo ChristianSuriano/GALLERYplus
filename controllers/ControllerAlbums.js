504ec6617e6683f8867a98caa2c50f6674fcfb2d
Miriana <miriana.zammito@gmail.com>
2024-05-23 00:18:47 +0200

    feat/fix(miriana_zammito): aggiunto update, export, e fixato import modelAlbum

--- controllers/ControllerAlbums.js
+++ controllers/ControllerAlbums.js
@@ -1,21 +1,44 @@
-class ControllerAlbums {
+import ModelAlbum from "../models/ModelAlbum";
+
+export default class ControllerAlbums {
     #albums = [];
 
-    createAlbum (title, description, date){
+    createAlbum(title, description, date) {
         const newAlbum = new ModelAlbum(title, description, date);
         this.#albums.push(newAlbum);
         return newAlbum;
     }
 
-    delete(){
+    delete() {
         this.#albums = this.#albums.filter(id => newAlbum.id !== id);
     }
 
-    addImage(id, image){ //id: float, image: Photo
-        this.#albums[id].listPhotos.push(image);    
+    addImage(id, image) { //id: float, image: Photo
+        this.#albums[id].listPhotos.push(image);
     }
 
-    deleteImage(id){
+    deleteImage(id) {
         this.#albums[id].listPhotos = this.#albums[id].listPhotos.filter((id) => id !== this.#albums[id].id);
+    }
+
+    update(id, title, description, date) {
+        const albumToUpdate = this.#albums.find(album => album.id === id);
+
+        if (!albumToUpdate) {
+            return null; // Restituisci null se l'album non è stato trovato
+        }
+
+        if (title !== undefined) {
+            albumToUpdate.title = title;
+        }
+
+        if (description !== undefined) {
+            albumToUpdate.description = description;
+        }
+        if (date !== undefined) {
+            albumToUpdate.date = date;
+        }
+
+        return albumToUpdate;
     }
 }
