d4149510e327695878910702507dac8b220d74c7
Miriana <miriana.zammito@gmail.com>
2024-05-23 00:10:10 +0200

    refactor(miriana_zammito): aggiunto import, export e generazione Id

--- models/ModelAlbum.js
+++ models/ModelAlbum.js
@@ -1,6 +1,7 @@
-class ModelAlbum {
+import Generate from './utils/Generate.js';
+export default class ModelAlbum {
   constructor(title, description, date) {
-    this.id = Math.random();
+    this.id = Generate.generateUUID();
     this.listPhotos = [];
     this.title = title;
     this.description = description;
