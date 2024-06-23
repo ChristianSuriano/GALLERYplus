0f59e70879136562affdbc7c8b06d1a0df869cde
Francesco Gaziano <141562843+ProfessionalGaziano@users.noreply.github.com>
2024-06-23 20:43:19 +0200

    Delete Album directory

--- Album/ModelAlbum.js
+++
@@ -1,9 +0,0 @@
-class ModelAlbum {
-  constructor(title, description, date) {
-    this.id = Generate.generateUUID();
-    this.listPhotos = [];
-    this.title = title;
-    this.description = description;
-    this.date = date;
-  }
-}
